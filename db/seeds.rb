0c64aef5c6d2d7ab2bf156175977f58c989584d7
brynknowles <bryn.knowles@gmail.com>
2020-10-17 16:07:42 -0400

    lab completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+10.times do
+    Artist.create(name: Faker::FunnyName.unique.name, bio: Faker::GreekPhilosophers.unique.quote)
+end
+
+10.times do
+    Genre.create(name: Faker::Music.unique.genre)
+end
+
+30.times do
+    Song.create(name: Faker::TvShows::TwinPeaks.unique.location, artist_id: Artist.all.sample.id, genre_id: Genre.all.sample.id)
+end
